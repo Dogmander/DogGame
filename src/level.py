969a9935254f6247c3d37ddf2111ccafe6db4eb8
Dogmander <34490166+Dogmander@users.noreply.github.com>
2023-01-04 12:28:10 -0500

    Level loading now uses platform filename format

--- src/level.py
+++ src/level.py
@@ -1,17 +1,15 @@
 # Import dependecies
-import gltf
 import sys
+from panda3d.core import Filename
 
 class Level:
     def __init__(self):
-        # Patch the loader to support GLTF
-        # gltf.patch_loader(loader)
 
         # Load model and reparent to render
-        try:
-            self.model = loader.loadModel(sys.argv[1]) # Load level from provided file
-        except IndexError:
-            self.model = loader.loadModel("assets/bam/test.bam") # Load default level if no level provided
+        if len(sys.argv) >= 2:
+            self.model = loader.loadModel(Filename.fromOsSpecific(sys.argv[1]))
+        else:
+            self.model = loader.loadModel("assets/bam/test.bam")
         
         self.model.reparentTo(render)
         self.model.set_z(0)
